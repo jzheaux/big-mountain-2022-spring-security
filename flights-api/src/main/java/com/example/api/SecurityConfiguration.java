f789d2b95548513a18fc9fecb03a688d9a4268f6
Josh Cummings <josh.cummings@gmail.com>
2022-11-09 13:47:27 -0700

    Change to EnableMethodSecurity

--- flights-api/src/main/java/com/example/api/SecurityConfiguration.java
+++ flights-api/src/main/java/com/example/api/SecurityConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.api;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
